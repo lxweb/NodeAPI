514dc4d70dbf24eebb86454a2fc23c210a9af0f3
lxweb102 <lxweb102@gmail.com>
2023-11-02 15:07:38 -0300

    Adding routes with controlers

--- src/routers/company.route.ts
+++ src/routers/company.route.ts
@@ -3,12 +3,14 @@ import express from "express"
 
 import { 
     getAllCompaniesWithEmployees, 
-    getCompanyWithEmployeesByName
+    getCompanyWithEmployeesByName,
+    createCompany
 } from '../controllers/company.controller'
 
 const router = express.Router()
 
 router.get('/', getAllCompaniesWithEmployees)
 router.get('/:name', getCompanyWithEmployeesByName)
+router.post('/', createCompany)
 
 export default router
